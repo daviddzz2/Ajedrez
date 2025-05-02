fd0a76b049eabffb628076101bdffde0997d9f8d
z1v0ware <dagarfer@myuax.com>
2025-05-02 13:51:13 +0200

    Tablero completo

--- tablero.py
+++ tablero.py
@@ -7,10 +7,23 @@ def inicializar_tablero():
     tablero[0][0] = tablero[0][7] = "T"  # Torres blancas
     tablero[7][0] = tablero[7][7] = "t"  # Torres negras
 
+    tablero[0][1] = tablero[0][6] = "C"  # Caballos blancos
+    tablero[7][1] = tablero[7][6] = "c"  # Caballos negros
+
+    tablero[0][2] = tablero[0][5] = "A"  # Alfiles blancos
+    tablero[7][2] = tablero[7][5] = "a"  # Alfiles negros
+
+    tablero[0][3] = "Q"  # Dama blanca
+    tablero[7][3] = "q"  # Dama negra
+
+    tablero[0][4] = "K"  # Rey blanco
+    tablero[7][4] = "k"  # Rey negro
+
     return tablero
 
 def mostrar_tablero(tablero):
-    print("    " + "   ".join(str(i+1) for i in range(8)))
+    print("\n" + " " * 9 + "TABLERO DE AJEDREZ")
+    print("\n    " + "   ".join(str(i+1) for i in range(8)))
     print("  +" + "---+" * 8)
     for i, fila in enumerate(tablero):
         fila_str = f"{i+1} |"
@@ -21,3 +34,5 @@ def mostrar_tablero(tablero):
                 fila_str += f" {celda} |"
         print(fila_str)
         print("  +" + "---+" * 8)
+
+mostrar_tablero(inicializar_tablero())
